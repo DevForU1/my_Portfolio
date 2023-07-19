3812e7bad41a060469b803c73c18f79a03684054
DevForU1 <olehpokotyliuk@gmail.com>
2023-07-19 05:31:26 +0300

    delete file

--- src/components/ProjectsCard/Projects.jsx
+++
@@ -1,7 +0,0 @@
-import classes from './Projects.module.scss'
-import { MediaCard } from '../Card/Card'
-export const Projects = (props) => {
-    return (
-            <MediaCard className={classes.mediaCard}/>
-    )
-}
